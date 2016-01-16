1aac66b197e405170be62e2af66a95d503dc3356
LazyTarget <peter.aslund@hotmail.com>
2016-01-16 01:39:51 +0100

    Imported assets

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log("LoadLevel() Name: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel() {
+		Debug.Log("LoadNextLevel() Loaded Level: " + Application.loadedLevelName);
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void Quit(){
+		Debug.Log("Quit()");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public static MusicPlayer Instance { get; private set; }
+
+	void Awake() {
+		//Debug.Log("MusicPlayer:Awake() ::" + GetInstanceID());
+		if (Instance != null) {
+			// Don't create new music players if one already exists
+			Debug.Log("MusicPlayer:Awake() Destroying extra instance of MusicPlayer ::" + GetInstanceID());
+			Destroy(gameObject);
+		}
+		else {
+			// Don't destroy the music player when changing scenes
+			Instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("MusicPlayer:Start() ::" + GetInstanceID());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
